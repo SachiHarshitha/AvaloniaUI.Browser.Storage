762af5abb3454b740eddf0c40c68dc9d35fd85c3
Sachi Harshitha Liyanagama <sachi.harshitha@live.com>
2025-05-29 20:44:01 +0200

    - Storage Extension to Serialize data to json

---
+++ AvaloniaUI.Browser.Storage/StorageExtensions.cs
@@ -0,0 +1,43 @@
+﻿using AvaloniaUI.Browser.Storage.Contracts;
+
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace AvaloniaUI.Browser.Storage;
+
+public static class StorageExtensions
+{
+    public static async ValueTask SetItemAsJsonAsync<T>(
+        this ILocalStorageService storage, string key, T value)
+    {
+        var json = JsonSerializer.Serialize(value);
+        await storage.SetItemAsync(key, json);
+    }
+
+    public static async ValueTask<T?> GetItemFromJsonAsync<T>(
+        this ILocalStorageService storage, string key)
+    {
+        var json = await storage.GetItemAsync(key);
+        if (string.IsNullOrEmpty(json))
+            return default;
+
+        return JsonSerializer.Deserialize<T>(json);
+    }
+
+    public static async ValueTask SetItemAsJsonAsync<T>(
+        this ISessionStorageService storage, string key, T value)
+    {
+        var json = JsonSerializer.Serialize(value);
+        await storage.SetItemAsync(key, json);
+    }
+
+    public static async ValueTask<T?> GetItemFromJsonAsync<T>(
+        this ISessionStorageService storage, string key)
+    {
+        var json = await storage.GetItemAsync(key);
+        if (string.IsNullOrEmpty(json))
+            return default;
+
+        return JsonSerializer.Deserialize<T>(json);
+    }
+}
