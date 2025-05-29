f43066de03ec300d36e360c49f83fe88e6a89192
Sachi Harshitha Liyanagama <sachi.harshitha@live.com>
2025-05-29 19:13:48 +0200

    - Update Readme and demo.

--- AvaloniaUI.Browser.Storage.Demo/ViewModels/MainViewModel.cs
+++ AvaloniaUI.Browser.Storage.Demo/ViewModels/MainViewModel.cs
@@ -1,33 +1,20 @@
-﻿using System.Threading.Tasks;
-
-using CommunityToolkit.Mvvm.ComponentModel;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace AvaloniaUI.Browser.Storage.Demo.ViewModels;
 
 public partial class MainViewModel : ViewModelBase
 {
-    private SessionStorageService _sessionStorageService = new SessionStorageService();
-    private LocalStorageService _localStorageService = new LocalStorageService();
-    public string Greeting => "Welcome to Avalonia!";
+    #region Fields
 
-    [RelayCommand]
-    public async Task SessionStorageCommand()
-    {
-        await _sessionStorageService.SetItemAsync("Session_Text", ValueToSetSessionStorage);
-        ValueFromSessionStorage = await _sessionStorageService.GetItemAsync("Session_Text");
-        await Task.CompletedTask;
-    }
-
-    [RelayCommand]
-    public async Task LocalStorageCommand()
-    {
-        // This is a placeholder for the local storage command logic.
-        // You can implement your local storage logic here.
-        await _localStorageService.SetItemAsync("Local_Text", ValueToSetLocalStorage);
-        ValueFromLocalStorage = await _localStorageService.GetItemAsync("Local_Text");
-        await Task.CompletedTask;
-    }
+    private readonly SessionStorageService _sessionStorageService;
+    private readonly LocalStorageService _localStorageService;
 
     [ObservableProperty]
     private string _valueToSetLocalStorage = string.Empty;
@@ -40,4 +27,122 @@ public partial class MainViewModel : ViewModelBase
 
     [ObservableProperty]
     private string _valueFromLocalStorage = string.Empty;
+
+    [ObservableProperty]
+    private ObservableCollection<Tuple<string, object>> _sessionStorageEntries = new ObservableCollection<Tuple<string, object>>();
+
+    [ObservableProperty]
+    private ObservableCollection<Tuple<string, object>> _localStorageEntries = new ObservableCollection<Tuple<string, object>>();
+
+    #endregion Fields
+
+    /// <summary>
+    /// Default constructor for MainViewModel.
+    /// </summary>
+    public MainViewModel()
+    {
+        // Initialize the session storage service and local storage service.
+        _sessionStorageService = new SessionStorageService();
+        _localStorageService = new LocalStorageService();
+        // Optionally, you can load initial data or perform setup here.
+        LoadInitialData();
+    }
+
+    #region Props
+
+    public string Greeting => "Welcome to AvaloniaUI Browser Storage Library";
+
+    #endregion Props
+
+    /// <summary>
+    /// Loads initial data from session and local storage.
+    /// </summary>
+    private void LoadInitialData()
+    {
+        GetSessionStorageEntries();
+        GetLocalStorageEntries();
+    }
+
+    [RelayCommand]
+    public async Task SessionStorageCommand()
+    {
+        var guid = Guid.NewGuid().ToString();
+        await _sessionStorageService.SetItemAsync(guid, ValueToSetSessionStorage);
+        ValueFromSessionStorage = await _sessionStorageService.GetItemAsync(guid);
+        GetSessionStorageEntries();
+        await Task.CompletedTask;
+    }
+
+    [RelayCommand]
+    public async Task LocalStorageCommand()
+    {
+        var guid = Guid.NewGuid().ToString();
+        await _localStorageService.SetItemAsync(guid, ValueToSetLocalStorage);
+        ValueFromLocalStorage = await _localStorageService.GetItemAsync(guid);
+        GetLocalStorageEntries();
+        await Task.CompletedTask;
+    }
+
+    [RelayCommand]
+    public async Task ClearSessionStorageAsync()
+    {
+        await _sessionStorageService.ClearAsync();
+        ValueFromSessionStorage = string.Empty;
+        SessionStorageEntries.Clear();
+    }
+
+    [RelayCommand]
+    public async Task ClearLocalStorageAsync()
+    {
+        await _localStorageService.ClearAsync();
+
+        ValueFromLocalStorage = string.Empty;
+        LocalStorageEntries.Clear();
+    }
+
+    [RelayCommand]
+    public async void GetSessionStorageEntries()
+    {
+        try
+        {
+            SessionStorageEntries.Clear();
+
+            var length = await _sessionStorageService.Length;
+
+            for (var i = 0; i < length; i++)
+            {
+                var key = await _sessionStorageService.KeyAsync(i);
+                if (string.IsNullOrEmpty(key)) continue;
+                var value = await _sessionStorageService.GetItemAsync(key);
+                SessionStorageEntries.Add(new Tuple<string, object>(key, value ?? "null"));
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Error retrieving session storage entries: {e.Message}");
+        }
+    }
+
+    [RelayCommand]
+    public async void GetLocalStorageEntries()
+    {
+        try
+        {
+            LocalStorageEntries.Clear();
+
+            var length = await _localStorageService.Length;
+
+            for (var i = 0; i < length; i++)
+            {
+                var key = await _localStorageService.KeyAsync(i);
+                if (string.IsNullOrEmpty(key)) continue;
+                var value = await _localStorageService.GetItemAsync(key);
+                LocalStorageEntries.Add(new Tuple<string, object>(key, value ?? "null"));
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Error retrieving local storage entries: {e.Message}");
+        }
+    }
 }
--- README.md
+++ README.md
@@ -1,1 +1,68 @@
 # AvaloniaUI.Browser.Storage
+
+This library provides a simple way to use browser storage in AvaloniaUI applications. It supports both local and session storage, allowing you to store key-value pairs that persist across sessions or only for the duration of the page load.
+
+## Sample Application
+
+![image](https://github.com/user-attachments/assets/32aa25af-08d2-4a3b-ad2d-8ea231c3e12d)
+
+
+## How to Use
+
+Currently , the library is in the early stages of development. To use it, you can clone the repository and reference the project in your AvaloniaUI application.
+
+1. Clone the repository:
+```bash
+   git clone https://github.com/SachiHarshitha/AvaloniaUI.Browser.Storage.git
+ ```
+
+2. Add a reference to the `AvaloniaUI.Browser.Storage` project in your AvaloniaUI application (Both in the base and startup projects).
+
+3. Add the following section to the Browser startup project (File : xxxx.Browser.csproj).
+
+```xml
+<!--Section :Direct Reference, Add following section if you refer the project directly-->
+	<ItemGroup>
+		<JsFiles Include="..\AvaloniaUI.Browser.Storage\wwwroot\js\**\*.*" />
+	</ItemGroup>
+
+	<Target Name="CopyJsFilesBeforeBuild" BeforeTargets="Build">
+		<Message Importance="high" Text="Copying JS files to wwwroot..." />
+		<Copy
+			SourceFiles="@(JsFiles)"
+			DestinationFolder="$(MSBuildProjectDirectory)\wwwroot\js\%(RecursiveDir)"
+			SkipUnchangedFiles="true" />
+	</Target>
+<!-- End Section-->
+```
+
+4. Add the required JSScript loading to your crossplatform project (File : xxxx.csproj).
+
+```csharp
+  if (OperatingSystem.IsBrowser())
+        {
+            await JSHost.ImportAsync("FuncLocalStorage", "/js/FuncLocalStorage.js");
+            await JSHost.ImportAsync("FuncSessionStorage", "/js/FuncSessionStorage.js");
+        }
+```
+
+5. Use the `LocalStorage` and `SessionStorage` classes to store and retrieve data in your AvaloniaUI application.
+```csharp
+	using AvaloniaUI.Browser.Storage;
+
+	.........
+
+	private SessionStorageService _sessionStorageService = new SessionStorageService();
+	private LocalStorageService _localStorageService = new LocalStorageService();
+
+	..........
+
+	# Session Storage Example
+	await _sessionStorageService.SetItemAsync("Session_Text", ValueToSetSessionStorage);
+    ValueFromSessionStorage = await _sessionStorageService.GetItemAsync("Session_Text");
+
+	...........
+	# Local Storage Example
+	await _localStorageService.SetItemAsync("Local_Text", ValueToSetLocalStorage);
+	ValueFromLocalStorage = await _localStorageService.GetItemAsync("Local_Text");
+```
